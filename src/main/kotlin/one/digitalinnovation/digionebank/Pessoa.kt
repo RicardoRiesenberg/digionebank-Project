8748eabbb6f743c6aedfd204cc733abf19b9f366
Ricardo Riesenberg <kadooberg@gmail.com>
2022-07-27 20:03:23 -0300

    create personal class

---
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -0,0 +1,13 @@
+package one.digitalinnovation.digionebank
+
+class Pessoa {
+    var nome: String = "Ricardo"
+    var cpf: String = "123.456.789-00"
+
+}
+fun main(){
+    val Ricardo = Pessoa()
+
+    println(Ricardo.nome)
+    println(Ricardo.cpf)
+}
