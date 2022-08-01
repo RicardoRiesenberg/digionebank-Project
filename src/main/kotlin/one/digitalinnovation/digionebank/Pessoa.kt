f33eaf4c5e65ac0a33e91f1ff78dbe2417bb5ae9
Ricardo Riesenberg <kadooberg@gmail.com>
2022-08-01 11:14:08 -0300

    update to talk about class members

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -2,16 +2,17 @@ package one.digitalinnovation.digionebank
 
 class Pessoa {
     var nome: String = "Ricardo"
-
     var cpf: String = "123.456.789-00"
         private set
 
+    constructor()
+
+    fun pessoaInfo() = "$nome e $cpf"
 
 }
 fun main(){
     val Ricardo = Pessoa()
-    println(Ricardo)
-    println(Ricardo.nome)
-    println(Ricardo.cpf)
+    println(Ricardo.pessoaInfo())
+
 
 }
