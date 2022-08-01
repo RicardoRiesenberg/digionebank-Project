d11fb02ddf9012f49fd02b14c898844e7c148684
Ricardo Riesenberg <kadooberg@gmail.com>
2022-08-01 10:58:50 -0300

    update to apply the private modify to set function or cpf propriety

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -2,12 +2,16 @@ package one.digitalinnovation.digionebank
 
 class Pessoa {
     var nome: String = "Ricardo"
+
     var cpf: String = "123.456.789-00"
+        private set
+
 
 }
 fun main(){
     val Ricardo = Pessoa()
-
+    println(Ricardo)
     println(Ricardo.nome)
     println(Ricardo.cpf)
+
 }
