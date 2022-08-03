f71789d7c48b1556d7f7b539b9b2f553a46b18c7
Ricardo Riesenberg <kadooberg@gmail.com>
2022-08-03 18:33:45 -0300

    create tests and report printer

---
+++ src/main/kotlin/one/digitalinnovation/digionebank/Analista.kt
@@ -0,0 +1,15 @@
+package one.digitalinnovation.digionebank
+
+import java.math.BigDecimal
+
+class Analista(
+    nome: String,
+    cpf: String,
+    salario: Double
+
+) : Funcionario(nome,cpf, salario){
+        override fun CalculoAuxilio() = salario * 0.1
+
+
+
+}
